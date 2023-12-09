d029ff1e47a8f75e8a8aefee9938d925e91db6c8
hafizdwp <hafizdwp@gmail.com>
2023-12-09 08:24:50 +0700

    WIP: Layouting main menu

--- lib/controller/side_menu_controller.dart
+++ lib/controller/side_menu_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class SideMenuController extends GetxController {
   RxInt index = 0.obs;
@@ -10,13 +11,7 @@ class SideMenuController extends GetxController {
   }
 
   var pages = [
-    Container(
-      color: Colors.red,
-      child: Text(
-        "Tes 1",
-        textAlign: TextAlign.center,
-      ),
-    ),
+    TodayScreen(),
     Container(
       color: Colors.green,
       child: Text(
@@ -25,4 +20,80 @@ class SideMenuController extends GetxController {
       ),
     )
   ];
+}
+
+class TodayScreen extends StatelessWidget {
+  const TodayScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final now = DateTime.now();
+    final formatter = DateFormat('EEEE, d MMMM');
+    final todayDateInString = formatter.format(now);
+
+    return Container(
+      color: Color(0xffe3e3e3),
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                    flex: 10,
+                    child: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.sunny),
+                              SizedBox(
+                                width: 12,
+                              ),
+                              Text("Today", style: TextStyle(
+                                fontSize: 24,
+                              ),)
+                            ],
+                          ),
+                          SizedBox(height: 8,),
+                          Text(todayDateInString),
+                        ],
+                      ),
+                    )),
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.sort),
+                          onPressed: () {},
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.group),
+                          onPressed: () {},
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.lightbulb),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Add a task',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
